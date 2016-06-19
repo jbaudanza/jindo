7b50900caace46b23c9802faec53c3efb785edba
Jonathan Baudanza <jon@jonb.org>
2016-06-18 19:21:39 -0700

    timestamps and avatars

--- js/chat/index.js
+++ js/chat/index.js
@@ -1,18 +1,64 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+const distanceOfTimeInWords = require('./distanceOfTimeInWords');
 
 function fetchProfile(identity) {
   return fetch("http://api.soundcloud.com/users/" + identity.userId  + "?client_id=929237b25472ca25f7977cef36ee6808").then(r => r.json());
 }
 
+
+class ChatMessage extends React.Component {
+  render() {
+    const styles = {
+      row: {
+        display: 'flex',
+        padding: 0,
+        margin: 0,
+        borderBottom: '1px solid #ccc',
+        marginTop: 5,
+        paddingBottom: 5,
+        listStyleType: 'none'
+      },
+      avatar: {
+        width: 35,
+        height: 35,
+        marginRight: 5
+      },
+      timestamp: {
+        color: '#ccc',
+        float: 'right'
+      }
+    };
+
+    return (
+      <li style={styles.row}>
+        <img className="avatar" style={styles.avatar} src="https://i1.sndcdn.com/avatars-000000172116-6885ee-large.jpg" />
+        <div style={{flex: 1}}>
+          <div className="header">
+            <b><a href="#">Jon Baudanza</a></b>
+            <span className="timestamp" style={styles.timestamp}>
+              {distanceOfTimeInWords(this.props.now - new Date(this.props.timestamp))}
+            </span>
+          </div>
+          <div className="body">
+            {this.props.message}
+          </div>
+        </div>
+      </li>
+    );
+  }
+}
+
+
 class ChatApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {list: [], connected: false};
+    this.state = {list: [], connected: false, message: ''};
 
     this.onSubmit = this.onSubmit.bind(this);
     this.onClickLogin = this.onClickLogin.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   componentWillMount() {
@@ -21,6 +67,9 @@ class ChatApp extends React.Component {
         .subscribe((list) => this.setState({list}));
 
     this.props.backend.connected.subscribe(connected => this.setState({connected}))
+
+    // TODO: this should update
+    this.setState({now: new Date()});
   }
 
   onClickLogin() {
@@ -37,26 +86,44 @@ class ChatApp extends React.Component {
   onSubmit(event) {
     event.preventDefault();
     const el = event.target.message;
-    this.props.backend.publish({message: el.value});
-    el.value = '';
+    if (el.value) {
+      this.props.backend.publish({message: el.value});
+      el.value = '';
+    }
+  }
+
+  onChange(event) {
+    this.setState({message: event.target.value});
   }
 
   render() {
+    const styles = {
+      list: {
+        padding: 0,
+        margin: 0
+      },
+      avatar: {
+        height: 40,
+        width: 40
+      }
+    };
+
     return (
-      <div>
+      <div className='chat-room'>
         Connected: <span>{String(this.state.connected)}</span>
 
-        <ul>
-          {this.state.list.map(e => <li key={e.id}>{e.message}</li>)}
+        <ul style={styles.list}>
+          {this.state.list.map(e => <ChatMessage key={e.id} now={this.state.now} {...e} />)}
         </ul>
-        <form onSubmit={this.onSubmit}>
-          <input name="message" />
-        </form>
 
         {
           this.state.profile ? (
             <div>
-              <img src={this.state.profile.avatar_url} />
+              <img className='avatar' src={this.state.profile.avatar_url} style={styles.avatar} />
+              <form onSubmit={this.onSubmit}>
+                <input name="message" onChange={this.onChange} value={this.state.message} />
+                <input type="submit" value="send" disabled={!this.state.message.trim()} />
+              </form>
             </div>
           ) : (
             <a href="#" onClick={this.onClickLogin}>
