7029c2969091668c5e68d52a816b3e45b8871457
Jonathan Baudanza <jon@jonb.org>
2016-06-23 14:17:22 -0700

    fix client/index.js compilation step

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,3 @@
-./node_modules/browserify/bin/cmd.js js/client/index.js -o public/client.js
+./node_modules/browserify/bin/cmd.js js/client/index.js -t [ babelify --presets [ react es2015 ] ] -o public/client.js
 ./node_modules/browserify/bin/cmd.js js/client/landing.js -t [ babelify --presets [ react es2015 ] ] -o public/landing.js
 ./node_modules/browserify/bin/cmd.js js/chat/index.js  -t [ babelify --presets [ react es2015 ] ] -o public/chat.js
