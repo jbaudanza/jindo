4452d4451ed24b3c0021d16f2a333b8ba788015d
Jonathan Baudanza <jon@jonb.org>
2016-06-28 16:48:45 -0700

    postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,4 @@
 ./node_modules/browserify/bin/cmd.js js/client/index.js -t [ babelify --presets [ react es2015 ] ] -o public/jindo.js
 ./node_modules/browserify/bin/cmd.js js/client/landing.js -t [ babelify --presets [ react es2015 ] ] -o public/landing.js
 ./node_modules/browserify/bin/cmd.js js/chat/index.js  -t [ babelify --presets [ react es2015 ] ] -o public/chat.js
+node ./js/precompile.js
